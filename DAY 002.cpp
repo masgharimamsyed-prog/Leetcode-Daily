71e2e5090bea5750e5a80ee8df5a3fce8cb2f989
Asghar Imam <masgharimamsyed@gmail.com>
2026-05-25 23:44:36 +0500

    Create DAY 002.cpp

---
+++ DAY 002.cpp
@@ -0,0 +1,56 @@
+/* 28. Find the Index of the First Occurrence in a String
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+ 
+
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+Example 2:
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+ 
+
+Constraints:
+
+1 <= haystack.length, needle.length <= 104
+haystack and needle consist of only lowercase English characters.*/
+
+
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        for(int i=0;i<haystack.length();i++){
+            if(haystack[i]==needle[0]){
+                bool found=true;
+                int index=i;
+            for(int j=0;j<needle.length();j++){
+               if(haystack[i]!=needle[j]){
+                 found=false;
+                 i=index;
+                   break;
+
+               }
+               i++;
+                          
+            }
+            if(found){
+                return index;
+            }
+            }
+        }
+
+        return -1;
+    }
+};
